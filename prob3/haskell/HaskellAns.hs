9eba165e9a40d91aa8396b538aba679b3674dc63
Gabriel Martinez <reitaka@gmail.com>
2013-07-19 15:14:14 -0700

    Initial Haskell solution to problem 3
    It works, but is much too slow for the given number

---
+++ prob3/haskell/HaskellAns.hs
@@ -0,0 +1,17 @@
+import Data.List
+import System.Environment
+
+main = do
+    args <- getArgs
+    let num = (read . head) args :: Int
+    print $ primeFactors num
+
+primeFactors :: Int -> [Int]
+primeFactors n = let candidates = primeCandidates n in
+    case find  (\x -> 0 == (mod n x)) candidates of
+        Just x -> (primeFactors x) ++ (primeFactors (div n x)) --Found a factor, recurse
+        Nothing -> [n] --No factors found, this is a prime number
+
+primeCandidates :: Int -> [Int]
+primeCandidates n = let top = (floor . (\x -> x / 2) . fromIntegral) n in
+    [top, top - 1 .. 2]
